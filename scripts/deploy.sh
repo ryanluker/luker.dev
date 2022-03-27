0d89ce672552ba4c999332f485dbef61b2d1ed86
Ryan Luker <ryanluker@outlook.com>
2022-03-27 17:46:54 +0000

    Add the placeholder post folders

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,7 @@
 echo "Deploying luker.dev!"
 cp -v ./src/home.html ./dist/home.html
 cp -v ./src/favicon.ico ./dist/favicon.ico
+cp -v -r ./src/2018 ./dist/2018
+cp -v -r ./src/2019 ./dist/2019
+cp -v -r ./src/2022 ./dist/2022
 echo "Finished deploying luker.dev!"
